066e4a759046087174d481b3dac69ec729ca110a
SoulOfTheAssassin <oli.liu798@gmail.com>
2025-07-09 11:06:24 +0800

    Added b_q

--- state.py
+++ state.py
@@ -86,9 +86,9 @@ class MenuState(State):
         super().__init__(manager, screen)
         self.center_x = self.screen.get_width() / 2
         self.center_y = self.screen.get_height() / 2
-        self.new_game_button = Button(self.center_x - 100, self.center_y - 100, 200, 50, "New Game", (0, 255, 0), (255, 255, 255))
-        self.options_button = Button(self.center_x - 100, self.center_y, 200, 50, "Options", (0, 0, 255), (255, 255, 255))
-        self.quit_button = Button(self.center_x - 100, self.center_y + 100, 200, 50, "Quit", (255, 0, 0), (255, 255, 255))
+        self.new_game_button = Button(self.center_x - 100, self.center_y - 100, 200, 50, "New Game", (0, 255, 0), (255, 255, 255), (255, 255, 255))
+        self.options_button = Button(self.center_x - 100, self.center_y, 200, 50, "Options", (0, 0, 255), (255, 255, 255), (255, 255, 255))
+        self.quit_button = Button(self.center_x - 100, self.center_y + 100, 200, 50, "Quit", (255, 0, 0), (255, 255, 255), (255, 255, 255))
 
     def handle_event(self, event):
         if event.type == _pygame.MOUSEBUTTONDOWN:
