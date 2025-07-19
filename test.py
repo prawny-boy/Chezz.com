446229b3e22ea94965e3b60917e106a0d65ce7f6
SoulOfTheAssassin <oli.liu798@gmail.com>
2025-07-19 12:27:37 +0800

    Added scaling in scaling.txt, also finished theme 1 (chezz)

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
-e = {'e':10}
-e.update({'f':20})
-print(e)
-# {'e': 10, 'f': 20}
+import pygame
+import os
+
+pygame.init()
+
+# Folder containing your sprite images
+SPRITES_FOLDER = os.path.join("Assets", "Sprites")
+
+def load_all_sprites(folder):
+    sprites = {}
+    for filename in os.listdir(folder):
+        if filename.endswith('.png'):
+            path = os.path.join(folder, filename)
+            image = pygame.image.load(path).convert_alpha()
+            sprites[filename] = image
+    return sprites
+
+# Load all sprites into a dictionary
+
